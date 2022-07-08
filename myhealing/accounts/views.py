5ed2c6acfaa6c23345479be32f1978b21e02f0aa
nneo <aisapple1030@naver.com>
2022-07-08 20:10:38 +0900

    keys.py 생성

--- myhealing/accounts/views.py
+++ myhealing/accounts/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-import requests, json
+import requests
+from keys import REST_API_KEY, REDIRECT_URL
 
 def kakaoLoginLogic(request):
-    _restApiKey = '0f35fc2805b692fffd8d94ff011d731a'
-    _redirectUrl = 'http://127.0.0.1:8000/accounts/kakaoLoginLogicRedirect'
+    _restApiKey = REST_API_KEY
+    _redirectUrl = REDIRECT_URL
     _url = f'https://kauth.kakao.com/oauth/authorize?client_id={_restApiKey}&redirect_uri={_redirectUrl}&response_type=code'
     return redirect(_url)
 
 def kakaoLoginLogicRedirect(request):
     _qs = request.GET['code'] # 인증 코드 가져오기
-    _restApiKey = '0f35fc2805b692fffd8d94ff011d731a'
-    _redirect_uri = 'http://127.0.0.1:8000/accounts/kakaoLoginLogicRedirect'
+    _restApiKey = REST_API_KEY
+    _redirect_uri = REDIRECT_URL
     _url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={_restApiKey}&redirect_uri={_redirect_uri}&code={_qs}'
     _res = requests.post(_url)
     _result = _res.json()
