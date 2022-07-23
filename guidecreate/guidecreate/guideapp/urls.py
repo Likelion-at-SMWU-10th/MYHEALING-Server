d8d3876da838e1dc18ddee101623ebfe9f40c337
Choi Yejin <amy350500@gmail.com>
2022-07-23 15:19:29 +0900

    글 상세조회 API 생성

--- guidecreate/guidecreate/guideapp/urls.py
+++ guidecreate/guidecreate/guideapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GuideList
+from .views import GuideList, GuideDetail
 
 urlpatterns = [
     path('guide/', GuideList.as_view()),
+    path('guide/<int:pk>', GuideDetail.as_view()),
 ]
--- guidecreate/guidecreate/guideapp/views.py
+++ guidecreate/guidecreate/guideapp/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 
 from .serializers import GuideSerializer, GuideListSerializer
 from .models import Guide
@@ -19,3 +20,15 @@ class GuideList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class GuideDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Guide.objects.get(pk=pk)
+        except Guide.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk): # guide
+        guide = self.get_object(pk)
+        serializer = GuideSerializer(guide)
+        return Response(serializer.data)
