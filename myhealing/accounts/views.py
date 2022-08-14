7cc62a59d69ce45eed50a5327d8070c5f60bfd0d
Choi Yejin <amy350500@gmail.com>
2022-08-14 22:42:33 +0900

    임시 커밋

--- myhealing/accounts/views.py
+++ myhealing/accounts/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth import login
 from django.core.files.base import ContentFile
+from rest_framework import status
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from myhealing.settings import SOCIAL_OUTH_CONFIG
 import requests, json
 from .models import User
-import urllib.request
 
 """
 인가코드 요청
@@ -52,11 +54,10 @@ class KakaoCallBackView(APIView):
         properties = profile_json.get("kakao_account").get("profile")
         nickname = properties.get("nickname") # 이름값
         profile_photo = properties.get("profile_image_url") # 프로필 사진
-        header_photo = properties.get("thumbnail_image_url") # 헤더(배경) 사진
         # return JsonResponse(data=properties, safe=False)
-        try:
+        try: # DB에 email이 존재하는지 확인
             user = User.objects.get(email=email)
-        except User.DoesNotExist:
+        except User.DoesNotExist: # DB에 없다면 계정 생성
             user = User.objects.create(
                 email = email,
                 nickname = nickname,
@@ -69,7 +70,28 @@ class KakaoCallBackView(APIView):
                     f"{nickname}-avatar", ContentFile(profile_photo_req.content)
                 )
         login(request, user)
-        return redirect(reverse('kakaologin')) # redirect page는 추후 변경.
+        # jwt token 접근
+        token = TokenObtainPairSerializer.get_token(user)
+        jwt_refresh_token = str(token)
+        jwt_access_token = str(token.access_token)
+        # profile_photo_req = requests.get(profile_photo)
+        res = Response(
+            {
+                "user": nickname,
+                "email": email,
+                # "profile_photo": ContentFile(profile_photo_req.content, encoding='utf-16'),
+                "message": "카카오 로그인에 성공하였습니다.",
+                "token": {
+                    "access": jwt_access_token,
+                    "refresh": jwt_refresh_token,
+                },
+            },
+            status = status.HTTP_200_OK
+        )
+        res.set_cookie("access", jwt_access_token, httponly=True)
+        res.set_cookie("refresh", jwt_refresh_token, httponly=True)
+        return res
+        # return redirect(reverse('kakaologin')) # redirect page는 추후 변경.
 
 
 class KakaoFinishView(APIView):
